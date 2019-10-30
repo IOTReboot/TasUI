98e45676a3868dbd638cb7a1ab4434bd4e6c4f81
Shantur Rathore <shantur.rathore@gmail.com>
2019-10-30 15:22:09 +0000

    Details : Basic switch and dimmer controls working

--- src/DeviceTypes/TasmotaDevice.js
+++ src/DeviceTypes/TasmotaDevice.js
@@ -1,4 +1,6 @@
-import { Box, Grid, Paper } from '@material-ui/core';
+import { withStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { green, blue } from '@material-ui/core/colors';
+import { Box, Grid, Paper, Container, Button, Slider } from '@material-ui/core';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -13,6 +15,63 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+
+const styles = theme => ({
+    imageContainer: {
+        padding: theme.spacing(2),
+        height: 200,
+        width: 200,
+    }, 
+    root: {
+        padding: theme.spacing(3, 2),
+        margin: theme.spacing(2),
+    },
+    detailsContainer: {
+        padding: theme.spacing(2),
+        margin: theme.spacing(2),
+    },
+})
+
+const onButtonTheme = createMuiTheme({
+    palette: {
+      primary: blue,
+    },
+  });
+
+const powerRegex = RegExp('POWER\D*')
+const dimmerRegex = RegExp('Dimmer\D*')
+
+
+const DimmerSlider = withStyles({
+    root: {
+      color: '#52af77',
+      height: 8,
+      width: 300
+    },
+    thumb: {
+      height: 24,
+      width: 24,
+      backgroundColor: '#fff',
+      border: '2px solid currentColor',
+      marginTop: -8,
+      marginLeft: -12,
+      '&:focus,&:hover,&$active': {
+        boxShadow: 'inherit',
+      },
+    },
+    active: {},
+    valueLabel: {
+      left: 'calc(-50% + 4px)',
+    },
+    track: {
+      height: 8,
+      borderRadius: 4,
+    },
+    rail: {
+      height: 8,
+      borderRadius: 4,
+    },
+  })(Slider);
 
 class TasmotaDevice extends Component {
 
@@ -48,6 +107,18 @@ class TasmotaDevice extends Component {
         })
     }
 
+    powerToggle(button) {
+        console.log('Toggle power ' + button)
+        this.deviceConnector.performCommandOnDevice(button + ' TOGGLE');
+        this.deviceConnector.getStatus0();
+    }
+
+    dimmerUpdate = (input) => (event, newValue) => {
+        console.log('Dimmer id : %O, value %O newValue %O', input, event, newValue);
+        this.deviceConnector.performCommandOnDevice(input + ' ' + newValue);
+        this.deviceConnector.getStatus0();
+    }
+
     renderTypeList() {
         return(
             <div style={{ width: '100%' }}>
@@ -69,6 +140,61 @@ class TasmotaDevice extends Component {
         return (
             <Svg src="https://raw.githubusercontent.com/arendst/Tasmota/development/tools/logo/TASMOTA_Symbol_Vector.svg" width="90%" height="90%" padding="20"/>
         )
+    }
+
+    renderDetailsControlsButtons() {
+        let buttons =  Object.entries(this.state.status0['StatusSTS']).filter(([key, value]) => {
+            if (powerRegex.test(key)) {
+                return [key, value];
+            }
+        })
+
+        console.log('DetailsControls buttons : %O', buttons)
+
+        return buttons.map(([key, value]) => {
+            if (value === 'ON') {
+                return (
+                    <ThemeProvider theme={onButtonTheme}>
+                        <Button variant="contained" color="primary" onClick={() => this.powerToggle(key)}>{key} {value}</Button>
+                    </ThemeProvider>
+                )
+            } else {
+                return (
+                    <Button variant="contained" onClick={() => this.powerToggle(key)}>{key} {value}</Button>
+                )
+            }
+        })
+    }
+
+    renderDetailsControlsDimmers() {
+        let dimmers =  Object.entries(this.state.status0['StatusSTS']).filter(([key, value]) => {
+            if (dimmerRegex.test(key)) {
+                return [key, value];
+            }
+        })
+
+        console.log('DetailsControls dimmers : %O', dimmers)
+
+        return dimmers.map(([key, value]) => {
+            return (
+                <div className={styles.imageContainer}>
+                    <DimmerSlider
+                        defaultValue={value}
+                        id={key}
+                        aria-labelledby={key + 'slider'}
+                        valueLabelDisplay="auto"
+                        step={1}
+                        min={1}
+                        max={100}
+                        onChangeCommitted={this.dimmerUpdate(key)}
+                    />
+                    <Typography id={key + 'slider'} gutterBottom variant="h5" justify="center">
+                        {key} : {value}
+                    </Typography>
+
+                </div>
+            )
+        })
     }
 
     renderDetailsStatuses() {
@@ -108,36 +234,40 @@ class TasmotaDevice extends Component {
             
 
         })
-    }
+    }  
 
     renderTypeDetails() {
         return(
-            <Paper width="sm">
-            <Grid container justify="center" alignItems="center" direction="column" width="sm" xs={16}>
-                {/* <Box display="flex"> */}
-                <Grid margin={20} xs={4}>
-                    <Paper width="200" height="200" align="center" padding="20" elevation={3}>
+            <Grid container justify="center" alignItems="center" direction="column" width="sm" xs={12}>
+                <Grid margin={20} className={styles.detailsContainer} >
+                    <Container fixed className={styles.imageContainer}>
                         {this.renderDetailsImage()}
-                    </Paper>
+                    </Container>
                 </Grid>
+                <Grid xs={12} className={styles.detailsContainer} >
+                    <Typography variant="h5">
+                        {this.state.displayName}
+                    </Typography>
+                </Grid>
+                <Grid xs={12} width="sm">
+                    {this.renderDetailsControlsDimmers()}
+                </Grid>
+                <Grid xs={12}>
+                    {this.renderDetailsControlsButtons()}
+                </Grid>
+
                 <Grid xs={6}>
-                    <Paper>
-                        <Typography>
-                            {this.state.displayName}
-                        </Typography>
-                    </Paper>
-                </Grid>
-                <Grid padding="20" margin={20} spacing={20} xs={6}>
                     {this.renderDetailsStatuses()}
                 </Grid>
                     <DeleteIcon/>
-                {/* </Box> */}
             </Grid>
-            </Paper>
         )
     }
 
     render() {
+        if (!this.state.status0) {
+            return null
+        }
         console.log('TasmotaDevice ' + this.props['renderType'])
         switch(this.props['renderType']) {
             case 'List':
@@ -159,4 +289,4 @@ class TasmotaDevice extends Component {
     }
 }
 
-export default TasmotaDevice;
+export default withStyles(styles)(TasmotaDevice);

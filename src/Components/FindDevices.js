87d1fc3409f5a0c438540a98fb4831fddbddff99
Shantur Rathore <shantur.rathore@gmail.com>
2019-11-25 16:51:02 +0000

    FindDevices : Add more buttons and fix width

--- src/Components/FindDevices.js
+++ src/Components/FindDevices.js
@@ -12,6 +12,9 @@ import React from 'react';
 import superagent from "superagent";
 import DeviceList from './DeviceList';
 import DisplayTypeButtons from './DisplayTypeButtons';
+import InfoIcon from '@material-ui/icons/Info';
+import CallToActionIcon from '@material-ui/icons/CallToAction';
+
 
 
 class FindDevices extends React.Component {
@@ -30,7 +33,7 @@ class FindDevices extends React.Component {
             searching: false,
             displayMode: "Table_Status"
         }
-        
+
     }
 
     componentWillMount() {
@@ -65,29 +68,32 @@ class FindDevices extends React.Component {
 
     calculateTotalIPs(ipFrom, ipTo) {
 
-        console.log('Calculate IPs From %s : To %s ', ipFrom , ipTo);
+        console.log('Calculate IPs From %s : To %s ', ipFrom, ipTo);
 
-        let from = new IPAddress.Address4(ipFrom);
-        let to = new IPAddress.Address4(ipTo);
+        if (ipFrom.split('.').length === 4 && ipTo.split('.').length === 4) {
 
-        console.log('From %O : To %O ', from.bigInteger().toString() , to.bigInteger().toString());
+            let from = new IPAddress.Address4(ipFrom);
+            let to = new IPAddress.Address4(ipTo);
 
-        this.setState({
-            totalAddresses: (to.bigInteger() - from.bigInteger() + 1),
-        })
+            // console.log('From %O : To %O ', from.bigInteger().toString() , to.bigInteger().toString());
 
-        console.log('Total Addresses : ' + (to.bigInteger() - from.bigInteger() + 1));
+            this.setState({
+                totalAddresses: (to.bigInteger() - from.bigInteger() + 1),
+            })
+
+            console.log('Total Addresses : ' + (to.bigInteger() - from.bigInteger() + 1));
+        }
     }
 
     onCommandResponse(args) {
         console.log(`${args.ip} : Present : ${args.success} Response : %O`, args.response);
         if (args.success) {
             // if (!this.props.deviceManager.isDeviceKnown(args.response.body.StatusNET.Mac)) {
-                this.props.deviceManager.addDiscoveredDevice(args.response.body.StatusNET.Mac, args.response.body);
-                this.setState({})
+            this.props.deviceManager.addDiscoveredDevice(args.response.body.StatusNET.Mac, args.response.body);
+            this.setState({})
             // }
         } else {
-            
+
         }
         this.ipsRequested = this.ipsRequested.filter(item => item !== args.ip);
         this.scanIps()
@@ -116,20 +122,20 @@ class FindDevices extends React.Component {
     }
 
     sendRequest(ip) {
-        var callback = function(err, response) {
-            this.onCommandResponse({key: this.cmnd, response: response, error: err, ip: this.ip, url: this.url, success: err ? false : true});
+        var callback = function (err, response) {
+            this.onCommandResponse({ key: this.cmnd, response: response, error: err, ip: this.ip, url: this.url, success: err ? false : true });
         }
 
         let cmnd = "Status 0"
         // let ip = IPAddress.Address4.fromBigInteger(ipNum).correctForm();
-        let url = 'http://' +  ip  + '/cm?cmnd=' + encodeURI(cmnd);
+        let url = 'http://' + ip + '/cm?cmnd=' + encodeURI(cmnd);
 
         superagent.get(url)
-        .timeout({
-            response: 5000,  // Wait 5 seconds for the server to start sending,
-            deadline: 60000, // but allow 1 minute for the file to finish loading.
-          })
-          .end(callback.bind({onCommandResponse: this.onCommandResponse.bind(this), ip: ip, url: url, cmnd: cmnd, success: true}))
+            .timeout({
+                response: 5000,  // Wait 5 seconds for the server to start sending,
+                deadline: 60000, // but allow 1 minute for the file to finish loading.
+            })
+            .end(callback.bind({ onCommandResponse: this.onCommandResponse.bind(this), ip: ip, url: url, cmnd: cmnd, success: true }))
 
     }
 
@@ -139,7 +145,7 @@ class FindDevices extends React.Component {
         let to = new IPAddress.Address4(this.state.ipTo).bigInteger();
 
 
-        for(let ipNum = from; ipNum <= to; ipNum++) {
+        for (let ipNum = from; ipNum <= to; ipNum++) {
             this.ipsToScan.push(IPAddress.Address4.fromBigInteger(ipNum).correctForm());
         }
         this.setState({
@@ -172,26 +178,43 @@ class FindDevices extends React.Component {
         this.setState({})
     }
 
-    knownDeviceButtons = [{
+    openDeviceSettings = (macAddress, event) => {
+        event.stopPropagation();
+        this.props.history.push('/settings/' + macAddress);
+    }
+
+    openDeviceConsole = (macAddress, event) => {
+        event.stopPropagation();
+        this.props.history.push('/console/' + macAddress);
+    }
+
+    commonButtons = [{
         toolTip: "Details",
         label: "details",
-        icon: <SettingsApplicationsIcon />,
+        icon: <InfoIcon />,
         onButtonClick: (mac, event) => this.openDeviceDetails(mac, event),
     },{
-        toolTip: "Delete", 
-        label: "delete", 
+        toolTip: "Settings",
+        label: "settings",
+        icon: <SettingsApplicationsIcon />,
+        onButtonClick: (mac, event) => this.openDeviceSettings(mac, event),
+    },{
+        toolTip: "Console", 
+        label: "console", 
+        icon: <CallToActionIcon />,
+        onButtonClick: (mac, event) => this.openDeviceConsole(mac, event),
+    }]
+
+    knownDeviceButtons = [...this.commonButtons, {
+        toolTip: "Delete",
+        label: "delete",
         icon: <DeleteIcon />,
         onButtonClick: (mac, event) => this.deleteDevice(mac, event),
     }]
 
-    newDeviceButtons = [{
-        toolTip: "Details",
-        label: "details",
-        icon: <SettingsApplicationsIcon />,
-        onButtonClick: (mac, event) => this.openDeviceDetails(mac, event),
-    },{
-        toolTip: "Add", 
-        label: "add", 
+    newDeviceButtons = [...this.commonButtons, {
+        toolTip: "Add",
+        label: "add",
         icon: <AddIcon />,
         onButtonClick: (mac, event) => this.addDevice(mac, event),
     }]
@@ -200,7 +223,7 @@ class FindDevices extends React.Component {
 
         let discoveredDevices = this.props.deviceManager.getDiscoveredDevices()
         let discoveredDevicesMacs = Object.keys(discoveredDevices)
-        
+
         let newDevices = discoveredDevicesMacs.filter((mac) => {
             return !this.props.deviceManager.isDeviceKnown(mac)
         }).reduce((obj, key) => {
@@ -218,49 +241,51 @@ class FindDevices extends React.Component {
 
 
         return (
-            <Box display="flex" flexGrow={1} flexDirection="column">
+            <Box display="flex" flexGrow={1} flexDirection="column" style={{ overflow: "visible", position: "absolute" }}>
                 <Box display="flex" alignItems="baseline" flexDirection="row">
-                <h1>Discover Active Devices</h1>
-                <DisplayTypeButtons displayMode={this.state.displayMode} setState={(state) => this.setState(state)} />
+                    <h1>Discover Active Devices</h1>
+                    <DisplayTypeButtons displayMode={this.state.displayMode} setState={(state) => this.setState(state)} />
                 </Box>
                 <Box display="flex" alignItems="baseline" flexDirection="row">
-                <TextField
-                    id="outlined-name"
-                    label="Start IP Address"
-                    placeholder="IP"
-                    margin="normal"
-                    variant="outlined"
-                    value={this.state.ipFrom}
-                    onChange={this.handleIPFromChange}
-                />
-                <TextField
-                    id="outlined-name"
-                    label="End IP Address"
-                    placeholder="IP"
-                    margin="normal"
-                    variant="outlined"
-                    value={this.state.ipTo}
-                    onChange={this.handleIPToChange}
-                />
-                {!this.state.searching ? 
-                    <Button variant="contained" margin="normal" onClick={() => this.handleFindClicked()}>Start Discovery</Button>
-                    : <Button variant="contained" margin="normal" onClick={() => this.handleStopClicked()} >Stop Discovery</Button>
-                }
+                    <TextField
+                        id="outlined-name"
+                        label="Start IP Address"
+                        placeholder="IP"
+                        margin="normal"
+                        variant="outlined"
+                        value={this.state.ipFrom}
+                        onChange={this.handleIPFromChange}
+                    />
+                    <TextField
+                        id="outlined-name"
+                        label="End IP Address"
+                        placeholder="IP"
+                        margin="normal"
+                        variant="outlined"
+                        value={this.state.ipTo}
+                        onChange={this.handleIPToChange}
+                    />
+                    {!this.state.searching ?
+                        <Button variant="contained" margin="normal" onClick={() => this.handleFindClicked()}>Start Discovery</Button>
+                        : <Button variant="contained" margin="normal" onClick={() => this.handleStopClicked()} >Stop Discovery</Button>
+                    }
                 </Box>
                 <Typography>
                     IPs to scan : {this.state.totalAddresses}
                 </Typography>
-                {this.state.searching ? 
+                {this.state.searching ?
                     <LinearProgress variant="buffer" value={this.state.numIpsCompleted} valueBuffer={this.state.numIpsRequested} />
-                    : null }
-                    
+                    : null}
+
 
                 {/* <h3>New Devices</h3> */}
-                <DeviceList 
+                <DeviceList
                     key="newDevices"
-                    displayMode={this.state.displayMode} 
-                    deviceSections={{ "New Devices": { devices: newDevices, itemButtons: this.newDeviceButtons},
-                                     "Saved Devices": { devices: knownDevices, itemButtons: this.knownDeviceButtons}}}
+                    displayMode={this.state.displayMode}
+                    deviceSections={{
+                        "New Devices": { devices: newDevices, itemButtons: this.newDeviceButtons },
+                        "Saved Devices": { devices: knownDevices, itemButtons: this.knownDeviceButtons }
+                    }}
                     deviceManager={this.props.deviceManager}
                 />
 

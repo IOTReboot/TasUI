92b2669ddc06119fee840e4792af80bdbf2a4af8
Shantur Rathore <shantur.rathore@gmail.com>
2020-01-16 10:36:26 +0000

    Relayout code repository

--- docker/build-container.sh
+++ docker/build-container.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build-container/build
 mkdir -p build-container/node_modules
-docker run -it --rm --name tasui-builder -v "$PWD":/tasui -v "$PWD/build-container/node_modules":/tasui/node_modules -v "$PWD/build-container/build":/tasui/build -w /tasui node:13.6 sh -c "npm install && npm run build"
+docker run -it --rm --name tasui-builder -v "$PWD/WebApp":/tasui -v "$PWD/build-container/node_modules":/tasui/node_modules -v "$PWD/build-container/build":/tasui/build -w /tasui node:13.6 sh -c "npm install && npm run build"
 docker build -f docker/Dockerfile .

6d7a28b9b52f53f7f8faf3035515d71d2d0173a0
Federico Leoni <effelle@gmail.com>
2020-01-16 13:51:07 -0300

    Delete build-container.sh

--- docker/build-container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p build-container/build
-mkdir -p build-container/node_modules
-docker run -it --rm --name tasui-builder -v "$PWD/WebApp":/tasui -v "$PWD/build-container/node_modules":/tasui/node_modules -v "$PWD/build-container/build":/tasui/build -w /tasui node:13.6 sh -c "npm install && npm run build"
-docker build -f docker/Dockerfile .
